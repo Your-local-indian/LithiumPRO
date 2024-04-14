9b33f22c0c2aa366974a031ea95c1d10b6ba3a12
Your local indian <a628472@outlook.com>
2024-04-14 11:56:47 -0500

    a

---
+++ search.js
@@ -0,0 +1,7 @@
+function  SEARCH(){
+   var Search=document.getElementById("Search").value;
+   var result=window.find(Search);
+   if (result == false) {
+    alert("Nothing found or already highlighted "
+  ); }
+  } 
